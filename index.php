656366b3397f5a245062a0ee0af9dca7fcc22a28
kopikkahmylvaganam <kopikkah98@gmail.com>
2022-06-27 14:20:07 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>details</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- CSS only -->
+
+</head>
+<body>
+<h1 style="text-align:center"> STUDENT REGISTRATION</h1>
+<hr>
+
+<div class= "container" style="background-color: darkslateblue;">
+
+<form >
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Student ID</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder=" Enter Student ID">
+  </div>
+
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">First name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter First name">
+  </div>
+
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Last name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Last name">
+  </div>
+
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Date of birth</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Date of birth">
+  </div>
+
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email">
+  </div>
+
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Department</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Department">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+
+<!-- JavaScript Bundle with Popper -->
+
+</body>
+</html>
