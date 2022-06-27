01dd656cc0d15461f5435eb470a5e9cfac0133e2
kopikkahmylvaganam <kopikkah98@gmail.com>
2022-06-27 15:35:49 +0530

    add points

--- index.php
+++ index.php
@@ -15,35 +15,35 @@
 
 <div class= "container" style="background-color: darkslateblue;">
 
-<form >
+<form action= "insert-student-data.php" method="post">
   <div class="form-group" style="margin-bottom:20px">
     <label for="exampleInputEmail1">Student ID</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder=" Enter Student ID">
+    <input type="text" class="form-control" name="student_id" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder=" Enter Student ID">
   </div>
 
   <div class="form-group" style="margin-bottom:20px">
     <label for="exampleInputEmail1">First name</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter First name">
+    <input type="text" class="form-control" name="firstname" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter First name">
   </div>
 
   <div class="form-group" style="margin-bottom:20px">
     <label for="exampleInputEmail1">Last name</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Last name">
+    <input type="text" class="form-control" name="lastname" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Last name">
   </div>
 
   <div class="form-group" style="margin-bottom:20px">
     <label for="exampleInputEmail1">Date of birth</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Date of birth">
+    <input type="text" class="form-control" name="dob" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Date of birth">
   </div>
 
   <div class="form-group" style="margin-bottom:20px">
     <label for="exampleInputEmail1">Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email">
+    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email">
   </div>
 
   <div class="form-group" style="margin-bottom:20px">
     <label for="exampleInputEmail1">Department</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Department">
+    <input type="text" class="form-control"  name="department" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Department">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
---
+++ insert-student-data.php
@@ -0,0 +1,29 @@
+<?php
+
+ $student_id=$_POST['student_id'];
+ $firstname=$_POST['firstname'];
+ $lastname=$_POST['lastname'];
+ $dob=$_POST['dob'];
+ $email=$_POST['email'];
+ $department=$_POST['department'];
+
+echo $student_id;
+ echo $firstname;
+ echo $lastname;
+ echo $dob;
+ echo $email;
+ echo $department;
+
+ 
+/*
+if($email == "hackmail.com"){
+
+    echo "Please you can't be registerd";
+}else{
+
+    echo "You can be registerd";
+}
+
+*/
+
+?>
